8804dbfa1e9b5226353be7d92bfd89a64194efdd
GrandhiAnanya <grandhi.ananya06@gmail.com>
2026-07-11 14:13:48 +0530

    modifed lab assignment 1 by including functions

--- Lab-Assignment-1/Q3.py
+++ Lab-Assignment-1/Q3.py
@@ -1,11 +1,14 @@
-list1 = [2, 5, 8, 10, 15, 20]
-list2 = [1, 5, 7, 10, 14, 20]
+def similarity(list1,list2):
+    count=0
 
-count=0
-
-for i in range(0,len(list1)):
-    for j in range(0,len(list2)):
+    for i in range(0,len(list1)):
+      for j in range(0,len(list2)):
         if list1[i]==list2[j]:
             count+=1
 
-print("number of elements similar in the list are : ",count)
+    print("number of elements similar in the list are : ",count)
+
+
+list1 = [2, 5, 8, 10, 15, 20]
+list2 = [1, 5, 7, 10, 14, 20]
+similarity(list1,list2)
--- Lab-Assignment-1/Q5.py
+++ Lab-Assignment-1/Q5.py
@@ -1,39 +1,51 @@
 import random
+def mean(list):
+   sum = 0
+   for i in list:
+     sum += i
 
+   mean = sum / len(list)
+   print("mean of the list: ",mean)
+
+def median(list):
+    if len(list)%2==0:
+     pos1=(len(list)//2)-1
+     pos2=(len(list)//2)
+     median=(list[pos1]+list[pos2])/2
+     print("\nmedian of the list: ",median)
+    else:
+      pos = (len(list)) // 2
+      median = list[pos]
+      print("\nmedian of the list: ",median)
+
+def mode(list):
+   frequency = {}
+
+   for i in list:
+     if i in frequency:
+        frequency[i] += 1
+     else:
+        frequency[i] = 1
+
+   max_freq = max(frequency.values())
+
+   mode = []
+   for key in frequency:
+     if frequency[key] == max_freq:
+        mode.append(key)
+   print("\nmode of the list: ",mode)
+   
+   
+  
 list=[]
 for i in range(100):
     list.append(random.randint(100, 150))
-print(list)
 
-sum = 0
-for i in list:
-    sum += i
 
-mean = sum / len(list)
-print("mean of the list: ",mean)
+mean(list)
+median(list)
+mode(list)
 
-if len(list)%2==0:
- pos1=(len(list)//2)-1
- pos2=(len(list)//2)
- median=(list[pos1]+list[pos2])/2
- print("\nmedian of the list: ",median)
-else:
- pos = (len(list)) // 2
- median = list[pos]
- print("\nmedian of the list: ",median)
 
-frequency = {}
 
-for i in list:
-    if i in frequency:
-        frequency[i] += 1
-    else:
-        frequency[i] = 1
 
-max_freq = max(frequency.values())
-
-mode = []
-for key in frequency:
-    if frequency[key] == max_freq:
-        mode.append(key)
-print("\nmode of the list: ",mode)
